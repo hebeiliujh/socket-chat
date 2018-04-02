1ad65cd938198e1224b22a6897737466d8eaffe5
Liu Jiahuan <liujiahuan@xuetangx.com>
2018-04-02 18:09:42 +0800

    project init

---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+ * @Author: liu.jiahuan 
+ * @Date: 2018-04-02 13:44:59 
+ * @Last Modified by: liu.jiahuan
+ * @Last Modified time: 2018-04-02 18:01:31
+ */
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path')
+
+var clientIp = null;
+
+app.get('/', function(req, res){
+  res.redirect('/chat.html');
+});
+
+app.use('/', express.static(path.join(__dirname,'./public')));
+
+var chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    console.log('a user connected');
+    // socket.emit('chat message', {
+    //     that: 'only'
+    //   , '/chat': 'will get'
+    // });
+    // chat.emit('chat message', {
+    //     everyone: 'in'
+    //   , '/chat': 'will get'
+    // });
+
+    socket.on('chat message', function(msg){
+      // socket.emit('chat message', msg);
+      
+      var address = socket.handshake.address
+      var time = socket.handshake.time
+      chat.emit('chat message', {
+        msg,
+        address,
+        time
+      });
+    });
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    // socket.on('hi!', function() {
+    // 	console.log('hi!')
+    // });
+  });
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
+  
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
---
+++ public/chat.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: liu.jiahuan 
+ * @Date: 2018-04-02 13:27:13 
+ * @Last Modified by: liu.jiahuan
+ * @Last Modified time: 2018-04-02 18:02:39
+ */
+
+$(function () {
+  // var socket = io();
+  var host = 'http://192.168.9.125:3000';
+  // host = 'http://127.0.0.1:3000'
+  var chat = io.connect(host + '/chat')
+  , news = io.connect(host + '/news');
+  $('form').submit(function(e){
+    chat.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  chat.on('chat message', function({msg, address, time}) {
+    $('#messages').append($('<li>').html('<div>(' + address + ' - ' + time + '): ' + msg + '</div>'));
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  news.on('news', function () {
+    news.emit('woot');
+  });
+});
